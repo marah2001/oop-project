385dc502db34ac229837c06607d3d07aa14d5b42
Marah Alsafadi <marahkinana21@gmail.com>
2024-11-18 20:02:31 +0200

    Initial commit for Assignment 16

---
+++ Assets/Scripts/Task18/Soldier.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assignment18
+{
+    public class Soldier : Character
+    {
+        
+        public Soldier(string name, int health, Position position) : base(name, health, position) { }
+
+        
+        public override void DisplayInfo()
+        {
+            Debug.Log("Soldier");
+            base.DisplayInfo();
+        }
+    }
+
+    public class Officer : Character
+    {
+       
+        public Officer(string name, int health, Position position) : base(name, health, position) { }
+
+      
+        public override void DisplayInfo()
+        {
+            Debug.Log("Officer");
+            base.DisplayInfo();
+        }
+    }
+}
