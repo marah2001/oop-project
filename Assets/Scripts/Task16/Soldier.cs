52d33bc54647690877908a3d3b011afd8be5f09a
Marah Alsafadi <marahkinana21@gmail.com>
2024-11-23 11:54:04 +0200

    this is assigment18

--- Assets/Scripts/Task16/Soldier.cs
+++ Assets/Scripts/Task16/Soldier.cs
@@ -17,16 +17,5 @@ namespace Assignment18
         }
     }
 
-    public class Officer : Character
-    {
-       
-        public Officer(string name, int health, Position position) : base(name, health, position) { }
-
-      
-        public override void DisplayInfo()
-        {
-            Debug.Log("Officer");
-            base.DisplayInfo();
-        }
-    }
+    
 }
